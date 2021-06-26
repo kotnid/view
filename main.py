d8c61143255da8a28ab8555593e012f2bb999e1d
kotnid <86507779+kotnid@users.noreply.github.com>
2021-06-26 13:24:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hi")
+import matplotlib.pyplot as plt
+import requests
+import bs4
+import os
+
+def draw(data):
+    plt.legend()
+    plt.show()
+
+
+def get_data():
+    print("ji")
+
+if os.path.exists("text.html"):
+    os.remove("text.html")
+
+header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    }
+    
+r = requests.get("https://ani.gamer.com.tw" , "html.parser" , headers= header)
+
+soup = bs4.BeautifulSoup(r.text , 'lxml')
+
+with open("text.html", "w", encoding="utf-8") as f:
+    f.write(str(soup.prettify()))
