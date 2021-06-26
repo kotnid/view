35a2573acb0839e40d7700e2b59d719bcb8d554f
kotnid <86507779+kotnid@users.noreply.github.com>
2021-06-26 20:30:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,122 @@
 import matplotlib.pyplot as plt
 import requests
 import bs4
-import os
+import dns
+from pymongo import MongoClient
+import datetime
+import time 
+import schedule
+import matplotlib.dates as mdates
+from matplotlib.pyplot import figure
 
-def draw(data):
-    plt.legend()
-    plt.show()
+cluster = MongoClient(
+    "mongodb+srv://bot:12345@data.3cfot.mongodb.net/data?retryWrites=true&w=majority"
+)
 
+db = cluster["anime_view"]
 
-def get_data():
-    print("ji")
-
-if os.path.exists("text.html"):
-    os.remove("text.html")
+collection = db["anime_view"]
 
 header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
     }
 
+
+
+def upload(name , num):
+    current_time = datetime.datetime.today().strftime("%Y/%m/%d")
+    data = [current_time,num]
+    myquery = {"_id" : name}
+    array = []
+
+    if collection.count_documents(myquery) == 0 :
+        add = {"_id" : name , "view":[data] }
+        collection.insert_one(add)
+
+    
+    query = {"_id" : name}
+    user = collection.find_one(query)
+    view_list = user["view"]
+    for vdata in view_list:
+        if vdata == data:
+            pass
+        else:
+            view_list.append(data)
+            collection.update_one({
+                "_id" : name
+            } , {"$set" : {
+                "view" : view_list
+            }})
+
+def main():
+    for data in datas:
+        details = data.find_all("p",{"class":""})
+
+        if len(list(details)) == 3:
+            if "萬" in list(details)[2].string:
+                num = int(float(list(details)[2].string.replace("萬","")))*10000
+                upload(list(details)[1].string,num)
+            elif "統計中" in list(details)[2].string:
+                num = 0
+                upload(list(details)[1].string,num) 
+            else:
+                num = int(list(details)[2].string)
+                upload(list(details)[1].string,num)
+             
+
+        if len(list(details)) == 2:
+            if "萬" in list(details)[1].string:
+                num = int(float(list(details)[1].string.replace("萬","")))*10000
+                upload(list(details)[0].string,num)
+            elif "統計中" in list(details)[1].string:
+                num = 0 
+                upload(list(details)[0].string,num)
+            else:
+                num = int(list(details)[1].string)
+                upload(list(details)[0].string,num)     
+        
+
+def chart():
+    plt.rcParams["figure.figsize"] = (10,10)
+    
+    datas = collection.find({})
+
+    for data in datas:
+        x = []
+        y = []
+
+        for view in data["view"]:
+            y.append(view[1])
+            time = datetime.datetime.strptime(view[0] , "%Y/%m/%d")
+            x.append(time)
+
+        plt.plot(x,y,label=data["_id"])
+
+    plt.gcf().autofmt_xdate()
+    myFmt = mdates.DateFormatter('%m/%d')
+    plt.gca().xaxis.set_major_formatter(myFmt)
+
+    plt.title("anime views")
+    plt.xlabel("time")
+    plt.ylabel("view")
+
+    plt.legend(prop={'size': 4})
+    plt.show()
+
+    plt.savefig("save.png")         
+
+
+def job():
+    main()
+    chart()
+
+
 r = requests.get("https://ani.gamer.com.tw" , "html.parser" , headers= header)
-
 soup = bs4.BeautifulSoup(r.text , 'lxml')
+datas = soup.find_all("a", {"class": "anime-card-block"})
 
-mydivs = soup.find_all("a", {"class": "anime-card-block"})
+schedule.every().day.at("00:00").do(job)
 
-find = mydivs[31].find_all("p",{"class":""})
-
-print(type(find))
-print(type(list(find)))
-print(list(find))
-
-
-print(list(find)[0].string)
-print(list(find)[1].string)
-print(list(find)[2].string)
-
-
-
-if "萬" in list(find)[2].string:
-   ans = list(find)[2].string.replace("萬","")
-   ans2 = int(float(ans))*10000
-   x = [1,2] 
-   y = [int(ans2),3000000]
-   test = [2700000,2900000]
-   plt.title('Anime view on ani.gamer.com.tw', fontsize=14)
-   plt.xlabel('date', fontsize=14)
-   plt.ylabel('view', fontsize=14)
-   plt.plot(x , test , label = "test")
-   plt.plot(x,y , label = list(find)[1].string)
-   plt.grid(True)
-   plt.legend()
-   plt.show()
-
+while True:
+    schedule.run_pending()
+    time.sleep(1) 
