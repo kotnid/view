d17e2c6da98a80f227617717ec8b3141144ed74f
kotnid <86507779+kotnid@users.noreply.github.com>
2021-06-26 14:35:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,38 @@ if os.path.exists("text.html"):
 header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
     }
-    
+
 r = requests.get("https://ani.gamer.com.tw" , "html.parser" , headers= header)
 
 soup = bs4.BeautifulSoup(r.text , 'lxml')
 
-with open("text.html", "w", encoding="utf-8") as f:
-    f.write(str(soup.prettify()))
+mydivs = soup.find_all("a", {"class": "anime-card-block"})
+
+find = mydivs[31].find_all("p",{"class":""})
+
+print(type(find))
+print(type(list(find)))
+print(list(find))
+
+
+print(list(find)[0].string)
+print(list(find)[1].string)
+print(list(find)[2].string)
+
+
+
+if "萬" in list(find)[2].string:
+   ans = list(find)[2].string.replace("萬","")
+   ans2 = int(float(ans))*10000
+   x = [1,2] 
+   y = [int(ans2),3000000]
+   test = [2700000,2900000]
+   plt.title('Anime view on ani.gamer.com.tw', fontsize=14)
+   plt.xlabel('date', fontsize=14)
+   plt.ylabel('view', fontsize=14)
+   plt.plot(x , test , label = "test")
+   plt.plot(x,y , label = list(find)[1].string)
+   plt.grid(True)
+   plt.legend()
+   plt.show()
+
